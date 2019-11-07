1f79991da35436c5eb9bce8e15eaa99abb2214bb
gabrdgs <gabbiematiias@gmail.com>
2019-11-07 17:48:23 -0300

    Exercicio Escada

--- animes.js
+++ animes.js
@@ -1,10 +1,10 @@
 
-alert ("Iremo fazer uma escada"); 
+alert ("Iremos fazer uma escada"); 
 let simbolo = prompt("Que simbolo será utilizado?");
 let quantidade = Number(prompt("Quantos degraus queremos?"));
 let escada = "";
 
 for (let e =0; e < quantidade; e++)
 {
-    console.log(e = escada+quantidade);
+    console.log(escada=escada+simbolo);
 }
