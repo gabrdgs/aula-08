cc57c4bb402410c52fdd4e112b85ce0343c05578
gabrdgs <gabbiematiias@gmail.com>
2019-11-06 21:05:03 -0300

    for e while

--- animes.js
+++ animes.js
@@ -1,10 +1,14 @@
 
 
-let animes= ['Death Note', 'Boku no Hero', 'Dragon Ball','Cavaleiros do Zodiaco','Naruto','SuperOnze'] ;
-let i=0;
+let animes = ['Death Note', 'Boku no Hero', 'Dragon Ball','Cavaleiros do Zodiaco','Naruto','SuperOnze'] ;
+let i = 0;
 
-while (i< animes.length)
-{
+//while (i< animes.length)
+//{
+//    console.log(animes[i]);
+//        i++; 
 
-
+//}
+for (let e=0; e< animes.length; e++){
+    console.log(animes[e])
 }
