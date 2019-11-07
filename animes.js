97904f2eadf4bad5ef8fac57d8f7e94ed2f2924b
gabrdgs <gabbiematiias@gmail.com>
2019-11-06 22:01:38 -0300

    for

--- animes.js
+++ animes.js
@@ -1,14 +1,10 @@
 
+alert ("Iremo fazer uma escada"); 
+let simbolo = prompt("Que simbolo será utilizado?");
+let quantidade = Number(prompt("Quantos degraus queremos?"));
+let escada = "";
 
-let animes = ['Death Note', 'Boku no Hero', 'Dragon Ball','Cavaleiros do Zodiaco','Naruto','SuperOnze'] ;
-let i = 0;
-
-//while (i< animes.length)
-//{
-//    console.log(animes[i]);
-//        i++; 
-
-//}
-for (let e=0; e< animes.length; e++){
-    console.log(animes[e])
+for (let e =0; e < quantidade; e++)
+{
+    console.log(e = escada+quantidade);
 }
